e1af79d23229c48d7da3f1fb0ec3b7f84ecdcb37
johnzenith <john.zenith.me@gmail.com>
2021-03-25 23:10:48 +0100

    finance request - form merging started

--- src/components/version-1.1.0/Finance/Requests/AddNewFinanceRequestForm.js
+++ src/components/version-1.1.0/Finance/Requests/AddNewFinanceRequestForm.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
 
+import StepperTitle from './../../Stepper/StepperTitle';
 import AddFinanceRequestForJobRelatedForm from './JobRelatedForm';
 import AddFinanceRequestForNonJobRelatedForm from './NonJobRelatedForm';
 
 function AddNewFinanceRequestForm() {
-    
+    const [requestType, setRequestType] = useState('');
+
+    const classes = makeStyles(() => ({
+        makeFormHidden: {
+            display: 'none',
+        },
+        formWrapper: {
+
+        }
+    }))();
+
+    const isJobRelatedForm    = /^job.*/gi.test(requestType);
+    const isNonJobRelatedForm = /^non.*/gi.test(requestType);
+
+    return (
+        <div>
+            <StepperTitle 
+                {...{
+                    title: 'Add Request Form',
+                    description: requestType,
+                }}
+            />
+
+            <div className={clsx( classes.formWrapper, {
+            [classes.makeFormHidden]: !isJobRelatedForm
+            })}>
+                <AddFinanceRequestForJobRelatedForm />
+            </div>
+
+            <div className={clsx( classes.formWrapper, {
+            [classes.makeFormHidden]: !isNonJobRelatedForm
+            })}>
+                <AddFinanceRequestForNonJobRelatedForm />
+            </div>
+        </div>
+    );
 }
 
 export default AddNewFinanceRequestForm;
